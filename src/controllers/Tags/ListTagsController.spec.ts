a24f24f41c883cac447440e30802a7ef65d1129a
Rubens Junqueira <rubens.junqueira@outlook.com>
2021-07-05 09:49:44 -0300

    teste: ListTagsController

---
+++ src/controllers/Tags/ListTagsController.spec.ts
@@ -0,0 +1,135 @@
+import connect from "../../database";
+import { getConnection } from "typeorm";
+import supertest from 'supertest';
+import { app } from "../../app";
+
+describe('ListTagsController', () => {
+    beforeAll(async () => {
+        await connect();
+    });
+
+    beforeEach(async () => {
+        const connection = getConnection();
+
+        await connection.dropDatabase();
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        const connection = getConnection();
+        await connection.close();
+    });
+
+    it('should not be able to list all tags if user is not authenticated with token', async () => {
+        const response = await supertest(app)
+            .get('/tags')
+
+        expect(response.status).toBe(401);
+        expect(response.body).toMatchObject({
+            error: 'Token is missing!'
+        });
+    });
+
+    it('should not be able to list all tags if user is not authenticated with valid token format', async () => {
+        const response = await supertest(app)
+            .get('/tags')
+            .set({ Authorization: `Bearer ` })
+
+        expect(response.status).toBe(400);
+        expect(response.body).toMatchObject({
+            error: 'Token invalid format!'
+        });
+    });
+
+    it('should not be able to list all tags if user sends a malformed token', async () => {
+        const response = await supertest(app)
+            .get('/tags')
+            .set({ Authorization: `Bearer 2ed40426-f1f1-5c8b-9e6c-d80bd7a11f6e` })
+            .send({
+                name: 'Ajuda'
+            });
+
+        expect(response.status).toBe(401);
+        expect(response.body).toMatchObject({
+            error: 'jwt malformed'
+        });
+    });
+
+    it('should not be able to list all tags if token signature is invalid', async () => {
+        const response = await supertest(app)
+            .get('/tags')
+            .set({ Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c` })
+            .send({
+                name: 'Ajuda'
+            });
+
+        expect(response.status).toBe(401);
+        expect(response.body).toMatchObject({
+            error: 'invalid signature'
+        });
+    });
+
+    it('should return status 204 if it is an empty list', async () => {
+        const { body: user } = await supertest(app)
+            .post('/users')
+            .send({
+                name: 'Bessie Welch',
+                email: 'dojgeluha@kuli.kp',
+                password: '123456',
+                admin: true
+            });
+
+        const { body: userAuthenticated } = await supertest(app)
+            .post('/users/authenticate')
+            .send({
+                email: user.email,
+                password: '123456'
+            });
+
+        const response = await supertest(app)
+            .get('/tags')
+            .set({ Authorization: `Bearer ${userAuthenticated.token}` });
+
+        expect(response.status).toBe(204);
+    });
+
+    it('should be return status 200 if list contains itens', async () => {
+        const { body: user } = await supertest(app)
+            .post('/users')
+            .send({
+                name: 'Bessie Welch',
+                email: 'dojgeluha@kuli.kp',
+                password: '123456',
+                admin: true
+            });
+
+        const { body: userAuthenticated } = await supertest(app)
+            .post('/users/authenticate')
+            .send({
+                email: user.email,
+                password: '123456'
+            });
+
+        await supertest(app)
+            .post('/tags')
+            .set({ Authorization: `Bearer ${userAuthenticated.token}` })
+            .send({
+                name: 'Ajuda'
+            });
+
+
+        const response = await supertest(app)
+            .get('/tags')
+            .set({ Authorization: `Bearer ${userAuthenticated.token}` });
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response.body).toMatchObject([{
+            id: expect.any(String),
+            name: 'Ajuda',
+            name_custom: '#ajuda',
+            created_at: expect.any(String),
+            updated_at: expect.any(String)
+        }]);
+    });
+});
